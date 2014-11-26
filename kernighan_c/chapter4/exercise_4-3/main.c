d21f0e4546d5853760f3362da9012a7991d224c1
dean <dbancajas@gmail.com>
2014-11-25 19:00:14 -0700

    xx

--- kernighan_c/chapter4/exercise_4-3/main.c
+++ kernighan_c/chapter4/exercise_4-3/main.c
@@ -4,12 +4,15 @@
 #define MAXOP 100
 #define NUMBER  '0'
 
+//Exercise 4-3. Given the basic framework, it's straightforward to extend the
+//calculator. Add the modulus (%) operator and provisions for negative numbers.
+//
+
+
 int getop(char []);
 void push(double);
 double pop(void);
 
- //reverse Polish calculator 
- //
 int main(void)
 {
 	int type;
