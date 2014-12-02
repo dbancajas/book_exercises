75217f121664bb3b23918dbd5f6cb5270a226cc4
Dean Ancajas <dbancajas@gmail.com>
2014-12-02 09:49:41 -0700

    wordcounter

---
+++ eckel_cpp/exercise_1-3/Wordcounter.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(void){
+
+	std::ifstream inp("textfile");
+
+	string str;
+	int count=0;
+
+	while(inp>>str) 
+		count++;
+
+	cout<<"Number of words: "<<count<<endl;
+
+
+	return 0;
+}
