b6549fac07c691cbf2bdabe83b0fe73a723ee59c
Dean Ancajas <dbancajas@gmail.com>
2014-12-03 10:28:31 -0700

    main

--- kernighan_c/chapter5/exercise_5-6/main.c
+++ kernighan_c/chapter5/exercise_5-6/main.c
@@ -2,6 +2,8 @@
 
 //Exercise 5-6: Rewrite appropriate programs from earlier chapters and exercises with pointers instead of array indexing. Good possibilities include getline (Chapters 1 and 4), atoi, itoa, (Chapters 2, 3 and 4) reverse (chapter 3) and strindex and getop (chapter 4).
 
+
+//Reverse from chapter 3
 void reverse(char *s){
 
 	char *t = s;
@@ -19,6 +21,23 @@ void reverse(char *s){
 	}
 
 
+}
+
+//getline from chapter 4 , not tested yet
+int getline2(char *s, int lim){
+	int c, i;
+
+	i=0;
+
+	while (--lim > 0 && (c=getchar()) !=EOF && c != '\n')
+		s[i++]=c;
+
+	if (c=='\n')
+		s[i++]=c;
+
+	s[i]='\0';
+
+	return i;
 }
 
 int main(void){
