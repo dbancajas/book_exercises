bbda4b809344136e78844a933de238cd59138170
Dean Ancajas <dbancajas@gmail.com>
2014-12-07 22:52:55 -0700

    exer 6

---
+++ koenig_cpp/chapter2/exercise_2-6/loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+//Exercise 2-6: What does the following code do?
+//
+//Answer: it prints 1-10 in its own line
+
+
+int main(void){
+
+	int i = 0;
+
+	while(i<10){
+		i+=1;
+		cout<<i<<endl;
+	}
+
+
+	return 0;
+}
