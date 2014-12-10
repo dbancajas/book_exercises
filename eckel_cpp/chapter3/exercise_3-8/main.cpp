65f69898da15693ed8e6bccebd221487b729a37f
Dean Michael Ancajas <dbancajas@gmail.com>
2014-12-10 11:33:44 -0700

    exercise 3-8

---
+++ eckel_cpp/chapter3/exercise_3-8/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+//Exercise 3-8: Write a program that uses all trigonometric functions.
+
+int main(void){
+	double angle=45;
+
+	cout<<"sin ("<<angle<<") = "<<sin(angle)<<endl;
+	cout<<"cos ("<<angle<<") = "<<cos(angle)<<endl;
+	cout<<"tan ("<<angle<<") = "<<tan(angle)<<endl;
+	cout<<"acos ("<<angle<<") = "<<acos(angle)<<endl;
+	
+
+	return 0;
+}
