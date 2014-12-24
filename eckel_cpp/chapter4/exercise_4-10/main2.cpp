72933f9373cf01e3ca90c1884615b546ccdcd896
Dean Ancajas <dbancajas@gmail.com>
2014-12-24 07:20:59 -0700

    describe the answer

--- eckel_cpp/chapter4/exercise_4-10/main2.cpp
+++ eckel_cpp/chapter4/exercise_4-10/main2.cpp
@@ -1,6 +1,18 @@
 
+#include<iostream>
+
+
+
 struct thestruct {
 	int myfunc(int a){
 		return a + 1;
 	}
 };
+
+
+int main(void){
+
+	thestruct b;
+	b.myfunc(2); 
+	return 0;
+}
--- eckel_cpp/chapter4/exercise_4-10/main2.s
+++
@@ -1,3 +0,0 @@
-	.file	"main2.cpp"
-	.ident	"GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
-	.section	.note.GNU-stack,"",@progbits
